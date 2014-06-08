0dc8573f5c962c2b5b2bdbcf0687f1269727c704
Pam <Pam@192.168.1.130>
2014-06-07 23:09:44 -0400

    dawbrosauce this is what i changed

--- src/com/me/calculadora/Main.java
+++ src/com/me/calculadora/Main.java
@@ -1,5 +1,15 @@
 package com.me.calculadora;
 
 public class Main {
-
+	public static void main (String[]dawbro) {
+	int i = 1;
+			
+ for (i = 1; i < 10; i++);
+	 while (i < 10){
+		 System.out.println("What upppppppp dawbro!!!");
+		 
+ }
+ 
+	 
+ }
 }
